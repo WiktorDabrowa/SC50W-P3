9e9939e3ba26662b3512a6238aa4d2ec4c52603a
unknown <wiktor.dabrowa98@gmail.com>
2022-08-28 14:32:07 +0200

    Finished Sending an Email with JSON and Inbox views

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
 
   //Sending Emails
-  document.querySelector('#submit-button').addEventListener('click', send_email);
+  document.querySelector('#compose-submit').addEventListener('click', send_email);
  
 });
 
@@ -37,27 +37,86 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Query the server for data
+fetch(`/emails/${mailbox}`)
+.then(response => response.json())
+.then(emails => {
+
+  // Print emails
+  //console.log(emails);
+
+  // Create a div for every email
+  const mailbox_div = document.querySelector('#emails-view')
+
+  for(const email of emails) {
+
+    // Get Particular data from Json TODO
+    let sender = email['sender'];
+    let subject = email['subject'];
+    let timestamp = email['timestamp'] 
+
+    // Create HTML elements
+    const email_container = document.createElement('div')
+    const title_div = document.createElement('div')
+    const timestamp_div = document.createElement('div')
+    const sender_div = document.createElement('div')
+
+    // Assign classes to divs
+    email_container.classList.add('email_container')
+    title_div.classList.add('title_div')
+    title_div.setAttribute('id',`${email['id']}`)
+    timestamp_div.classList.add('timestamp_div')
+    sender_div.classList.add('sender_div')
+    
+    // Populate divs with data
+    title_div.innerText = `Subject: ${String(subject)}`
+    sender_div.innerText = `Sent by: ${String(sender)}`
+    timestamp_div.innerHTML = String(timestamp)
+    if (email['read'] === true) {
+      email_container.style.backgroundColor = '#8080803d'
+    } else {
+      email_container.style.backgroundColor = 'White'
+    }
+
+    // Add elements to document
+    email_container.append(title_div)
+    email_container.append(sender_div)
+    email_container.append(timestamp_div)
+    mailbox_div.append(email_container)
+  
+    }
+    
+});
 }
+
 
 function send_email() {
 
-  const recipents = document.querySelector('#compose-recipients').value;
-  const subject = document.querySelector('#compose-subject').value;
-  const body = document.querySelector('#compose-body').value;
-
-  console.log(body)
+  // Set Variables to send in JSON
+  const mail_recipents = document.querySelector('#compose-recipients').value;
+  const mail_subject = document.querySelector('#compose-subject').value;
+  const mail_body = document.querySelector('#compose-body').value;
   
+  // Send data to the server
   fetch('/emails', {
      method: 'POST',
+     headers:{
+      'Content-type' : 'application/json'
+     },
      body: JSON.stringify({
-         recipients: String(recipents),
-         subject: String(subject),
-         body: String(body)
+         recipients: mail_recipents,
+         subject: mail_subject,
+         body: mail_body,
      })
    })
    .then(response => response.json())
    .then(result => {
+
      // Print result
      console.log(result);
    });
+
+   // Load the 'sent' Mailbox
+   load_mailbox('sent')
 }
